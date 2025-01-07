8e11b8793bd2232d1fd2e425cdcfb7900c4700b6
Hrushiingole <hrishikeshingole532@gmail.com>
2025-01-07 14:34:35 +0530

    making products page

--- src/main/java/com/ecom/Shopping_Cart/Controller/HomeController.java
+++ src/main/java/com/ecom/Shopping_Cart/Controller/HomeController.java
@@ -18,4 +18,9 @@ public class HomeController {
     public String register() {
         return "register";
     }
+
+    @GetMapping("/products")
+    public String products() {
+        return "product";
+    }
 }
